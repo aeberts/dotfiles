96d31d3d3a5936a96adac87342f76b3f32cf5351
Alex Eberts <alex.eberts@gmail.com>
2021-12-20 21:40:28 -0800

    Changed brew install script, added emacs-profiles

---
+++ dot_emacs-profiles.el
@@ -0,0 +1,20 @@
+;; Emacs startup is configured with the Chemacs2 bootloader
+;; https://https://github.com/plexus/chemacs2
+;; 
+;; Select which profile to use by editing the file ~/.emacs-profile
+;; 
+;; $ echo 'doom' > ~/.emacs-profile
+;; $ echo 'spacemacs' > ~/.emacs-profile
+;;
+(
+("default" . ((user-emacs-directory . "~/.doom-emacs")
+	      (env . (("DOOMDIR" . "~/.doom.d")))))
+
+("doom" . ((user-emacs-directory . "~/.doom-emacs")
+	      (env . (("DOOMDIR" . "~/.doom.d")))))
+
+("spacemacs" . ((user-emacs-directory . "~/.spacemacs-emacs")
+	      (env . (("SPACEMACSDIR" . "~/.spacemacs.d")))))
+
+("vanilla" . ((user-emacs-directory . "~/.emacs.default")))
+)
